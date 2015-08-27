fdcdc5b2bc16ee4d999c5249518e882415285a48
mempko <mempko@gmail.com>
2015-08-27 13:19:09 -0500

    added user model
    Ignore-this: 3181e31138b1504809b9eeccc47c2bcc
    
    darcs-hash:270664fa11b6f3291a60c41366214fd04eb6c931

--- src/muda.h
+++ src/muda.h
@@ -10,7 +10,7 @@
 #include "types.h"
 #include "roles.h"
 
-namespace mempko { namespace muda { namespace model{ 
+namespace mempko { namespace muda { namespace model { 
 
 	class muda : public role::modifable_text_and_notify<muda>
     {
@@ -26,6 +26,44 @@ namespace mempko { namespace muda { namespace model{
                 void serialize(Archive & ar, const unsigned int version)
                 {
                     ar & make_nvp("text", _text);
+                }
+    };
+
+    class text_value : public role::modifable_text_and_notify<text_value>
+    {
+        public:
+            const text_type& text() const { return _text;}
+            const void text(const text_type& t) { _text = t;}
+        private:
+            text_type _text;
+
+        private:
+            template<class Archive>
+                void serialize(Archive & ar, const unsigned int version)
+                {
+                    ar & make_nvp("text", _text);
+                }
+    };
+
+    class user
+    {
+        public:
+            const text_value& name() const { return _name;}
+            text_value& name() { return _name;}
+
+            const text_value& email() const { return _email;}
+            text_value& email() { return _email;}
+
+        private:
+            text_value _name;
+            text_value _email;
+        private:
+
+            template<class Archive>
+                void serialize(Archive & ar, const unsigned int version)
+                {
+                    ar & make_nvp("name", _name);
+                    ar & make_nvp("email", _email);
                 }
     };
 
